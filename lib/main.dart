bd5f76233860228303141b1e8b6398a9142feb6b
rariyuda <kjowo123@gmail.com>
2023-05-30 22:51:01 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp());
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
