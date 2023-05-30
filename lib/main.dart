bd87c50011ee8a707ade5c978366ae810a88e8e6
rariyuda <kjowo123@gmail.com>
2023-05-30 23:37:20 +0700

    mapping and routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/pages/choose_location.dart';
+import 'package:world_time/pages/home.dart';
+import 'package:world_time/pages/loading.dart';
 
-void main() {
-  runApp(const MaterialApp());
-}
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location':(context) => ChooseLocation(),
+  },
+));
---
+++ lib/pages/choose_location.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ChooseLocation extends StatefulWidget {
+  const ChooseLocation({super.key});
+
+  @override
+  State<ChooseLocation> createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function run');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ElevatedButton(onPressed: (){
+        setState(() {
+          counter += 1;
+        });
+      }, 
+      child: Text('Counter is $counter',),
+      ),
+    );
+  }
+}
