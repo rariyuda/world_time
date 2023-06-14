6759b7cba845084faafc8a567e490b951ba6f043
rariyuda <kjowo123@gmail.com>
2023-06-14 23:00:35 +0700

    remove getData in choose location

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -11,19 +11,6 @@ class _ChooseLocationState extends State<ChooseLocation> {
 
   int counter = 0;
 
-  void getData() async {
-    //simulate network request for a username
-    String username = await Future.delayed(const Duration(seconds: 3), (){
-      return 'yosan';
-    });
-
-    // simulate network request to get bio of the username
-    String bio = await Future.delayed(const Duration(seconds: 2), (){
-      return 'Meat eaters, gamers & bug collector';
-    });
-    // print('$username - $bio');
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
